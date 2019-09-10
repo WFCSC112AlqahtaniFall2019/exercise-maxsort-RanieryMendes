70ecb66a9e2c9d5b302d53248dca2b9c90b1b2b7
ranierymendes <mendrc18@wfu.edu>
2019-09-10 15:12:04 -0400

    Commit #1

--- maxSort.cpp
+++ maxSort.cpp
@@ -10,21 +10,27 @@ void print(const vector<int>& list);
 int main() {
 
     vector<int> v(5);  // vector to be sorted
-    // initialize v
-    /* your code here */
+
+     v = {6,1,3,2,1};  //initialize v
 
 
     // print the unsorted v
-    /* your code here */
 
+    cout << "The unsorted vector is: ";
+
+    for (int i = 0; i < v.size(); i++){
+
+        cout << v.at(i) << " ";
+    }
+ cout << endl;
 
     // sort v
-    /* your code here */
 
+    maxSort(v);
 
     // print the sorted v
-    /* your code here */
 
+    print(v);
 
     return 0;
 }
@@ -33,11 +39,18 @@ int main() {
 
 //Max sort function
 void maxSort(vector<int>& list){
-    for(int i=list.size()-1;i>=0;i--){
-        /*complete the code*/
+
+    for(int i=list.size()-1;i > =0; i--){
+
+       int current_max = 0;
 
         for(int j=0;j<=i;j++){
-            /*complete the code*/
+
+             if (list.at(j) > list.at(current_max)){
+                 current_max = j;
+
+                 swap(list.at(current_max), list.at(i));
+             }
 
         }
         /*complete the code*/
@@ -46,11 +59,24 @@ void maxSort(vector<int>& list){
 
 // swap function
 void swap(int& a, int&b){
-    /*your code here*/
+
+    int temp = a;
+
+    a = b;
+
+    b = temp;
+
+
 }
 
 // print function
 void print(const vector<int>& list){
-    /*your code here*/
+
+    cout << "List is sorted: ";
+
+    for(int i = 0; i < list.size(); i++)
+    {
+        cout << list.at(i) << " ";
+    }
 }
 
